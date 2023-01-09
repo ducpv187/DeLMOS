1c9ee1d62f7981710d71bc22557fbd9393ad7438
Ducpham <ducpv@beetsoft.com>
2023-01-09 15:21:15 +0700

    fix js modal

--- common/js/main.js
+++ common/js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-    $(".btn-blue").click(function() {
-      $(this).parents().find(".modal__popup").addClass('block');
-      $(this).parents().find(".modal__content").addClass('show');
-      $(this).parents().find(".modal__close").removeClass('show');
-    });
-    $(".modal_close").click(function() {
-        $(this).parents().find(".modal__popup").removeClass('block');
-      });
+  $(".btn-blue").click(function() {
+    $(this).parents().find(".modal__popup").addClass('block');
+    $(this).parents().find(".modal__content").addClass('show');
+    $(this).parents().find(".modal__close").removeClass('show');
+  });
+  $(".modal_close").click(function() {
+    $(this).parents().find(".modal__popup").removeClass('block');
+  });
+  $(".modal__content ").click(function(e) {
+    e.stopPropagation(e);
+  });
+  $(".modal__popup").click(function() {
+    $(this).removeClass('block');
+  });
 });
