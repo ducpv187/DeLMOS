7fd65e4f88595707276d4ec368c4c0a097ad24c9
Ducpham <ducpv@beetsoft.com>
2023-02-02 15:12:21 +0700

    upcode page forgot password

--- common/js/main.js
+++ common/js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     $(this).parents().find(".account__drop").removeClass('active_drop');   
   })
 
-  //cpn : show-hidden password
+  //page form register :show-hidden password
   $(".input__password .icon").click(function() { 
     let typePassword = $(this).parents().find(".input__password .input").attr("type");    
     if(typePassword == "password"){
@@ -46,37 +46,75 @@ $(document).ready(function() {
     $(this).siblings().removeClass("d-none");
   })
 
+  
   //page forgot password : show-hidden icon
   $(".form__reset .icon-show").click(function() {     
     $(this).addClass("d-none");
     $(this).siblings().addClass("d-block");
   })
+
   $(".form__reset .icon-hidden").click(function() {     
     $(this).removeClass("d-block");
     $(this).siblings().removeClass("d-none");
   })
 
-  //validate form
+ //page forgot password : show-hidden password
+  $(".js__newpass .icon").click(function() { 
+    let typeNewPassword = $(this).parents().find(".js__newpass .input").attr("type");    
+    
+    if(typeNewPassword == "password"){
+      $(this).parents().find(".js__newpass .input").attr('type', 'text');       
+    }
+    else {
+      $(this).parents().find(".js__newpass .input").attr('type', 'password');      
+    }
+  })
+
+  $(".js__resetpass .icon").click(function() { 
+    let typeRePassword = $(this).parents().find(".js__resetpass .input").attr("type");    
+ 
+    if(typeRePassword == "password"){
+      $(this).parents().find(".js__resetpass .input").attr('type', 'text');       
+    }
+    else {
+      $(this).parents().find(".js__resetpass .input").attr('type', 'password');      
+    }
+  })
+
+  // $(".form__reset .icon").click(function() { 
+  //   let typeNewPassword = $(this).parents().find(".js__newpass .input").attr("type"); 
+  //   let typeRePassword = $(this).parents().find(".js__resetpass .input").attr("type");    
+ 
+  //   if(typeNewPassword == "password" || typeRePassword == "password"){
+  //     $(this).parents().find(".input").attr('type', 'text');       
+  //   }
+  //   else {
+  //     $(this).parents().find(".input").attr('type', 'password');      
+  //   }
+  // })
+
+
+  //Validate form
   $('#my__form').validate({
-      rules: {
-        email: "required",
-        password: "required",
-        number: "required",
-        staffNameLastName: "required",
-        staffNameFirstName: "required",
-        staffNameLastNameKana: "required",
-        staffNameFirstNameKana: "required",
-        // acceptPolicy: "required",
-      },
-      messages: {
-        email: "メールアドレスを入力してください。",
-        password: "メールアドレスを入力してください。", 
-        number: "メールアドレスを入力してください。", 
-        staffNameLastName: "「姓」を入力してください。", 
-        staffNameFirstName: "「姓」を入力してください。", 
-        staffNameLastNameKana: "「セイ」を入力してください。", 
-        staffNameFirstNameKana: "「セイ」を入力してください。", 
-        // acceptPolicy: "利用規約とプライバシーポリシーに同意の上チェックを入れてください。", 
-      },
-    });
+    rules: {
+      email: "required",
+      password: "required",
+      number: "required",
+      staffNameLastName: "required",
+      staffNameFirstName: "required",
+      staffNameLastNameKana: "required",
+      staffNameFirstNameKana: "required",
+      // acceptPolicy: "required",
+    },
+    messages: {
+      email: "メールアドレスを入力してください。",
+      password: "メールアドレスを入力してください。", 
+      number: "メールアドレスを入力してください。", 
+      staffNameLastName: "「姓」を入力してください。", 
+      staffNameFirstName: "「姓」を入力してください。", 
+      staffNameLastNameKana: "「セイ」を入力してください。", 
+      staffNameFirstNameKana: "「セイ」を入力してください。", 
+      // acceptPolicy: "利用規約とプライバシーポリシーに同意の上チェックを入れてください。", 
+    },
+  });
 });
