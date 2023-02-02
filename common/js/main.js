61171fba5337fdf7865e6d2577fdca62caebb256
Ducpham <ducpv@beetsoft.com>
2023-02-02 11:32:25 +0700

    fix font family Toppage

--- common/js/main.js
+++ common/js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     $(this).parents().find(".account__drop").removeClass('active_drop');   
   })
 
-  //show-hidden password
+  //cpn : show-hidden password
   $(".input__password .icon").click(function() { 
     let typePassword = $(this).parents().find(".input__password .input").attr("type");    
     if(typePassword == "password"){
@@ -35,7 +35,7 @@ $(document).ready(function() {
     }
   })
   
-  //show-hidden icon  
+  //page form register : show-hidden icon  
   $(".input__password .icon .icon-hidden").click(function() {     
     $(this).addClass("d-none");
     $(this).siblings().addClass("d-block");
@@ -46,27 +46,37 @@ $(document).ready(function() {
     $(this).siblings().removeClass("d-none");
   })
 
-   //validate form
- $('#my__form').validate({
-    rules: {
-      email: "required",
-      password: "required",
-      number: "required",
-      staffNameLastName: "required",
-      staffNameFirstName: "required",
-      staffNameLastNameKana: "required",
-      staffNameFirstNameKana: "required",
-      // acceptPolicy: "required",
-    },
-    messages: {
-      email: "メールアドレスを入力してください。",
-      password: "メールアドレスを入力してください。", 
-      number: "メールアドレスを入力してください。", 
-      staffNameLastName: "「姓」を入力してください。", 
-      staffNameFirstName: "「姓」を入力してください。", 
-      staffNameLastNameKana: "「セイ」を入力してください。", 
-      staffNameFirstNameKana: "「セイ」を入力してください。", 
-      // acceptPolicy: "利用規約とプライバシーポリシーに同意の上チェックを入れてください。", 
-    },
-  });
+  //page forgot password : show-hidden icon
+  $(".form__reset .icon-show").click(function() {     
+    $(this).addClass("d-none");
+    $(this).siblings().addClass("d-block");
+  })
+  $(".form__reset .icon-hidden").click(function() {     
+    $(this).removeClass("d-block");
+    $(this).siblings().removeClass("d-none");
+  })
+
+  //validate form
+  $('#my__form').validate({
+      rules: {
+        email: "required",
+        password: "required",
+        number: "required",
+        staffNameLastName: "required",
+        staffNameFirstName: "required",
+        staffNameLastNameKana: "required",
+        staffNameFirstNameKana: "required",
+        // acceptPolicy: "required",
+      },
+      messages: {
+        email: "メールアドレスを入力してください。",
+        password: "メールアドレスを入力してください。", 
+        number: "メールアドレスを入力してください。", 
+        staffNameLastName: "「姓」を入力してください。", 
+        staffNameFirstName: "「姓」を入力してください。", 
+        staffNameLastNameKana: "「セイ」を入力してください。", 
+        staffNameFirstNameKana: "「セイ」を入力してください。", 
+        // acceptPolicy: "利用規約とプライバシーポリシーに同意の上チェックを入れてください。", 
+      },
+    });
 });
