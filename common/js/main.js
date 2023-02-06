ea5a10e57349167585c66283542493e34f7a0ff9
Ducpham <ducpv@beetsoft.com>
2023-02-06 09:40:46 +0700

    up code page archive

--- common/js/main.js
+++ common/js/main.js
@@ -97,8 +97,8 @@ $(document).ready(function() {
   //page list archive category
   $("input[type=radio]").click(function() {  
     let val = $(this).val();
-    $('.cate-items').removeClass('show');
-    $('#'+val).addClass('show')
+    $('.cate-items').removeClass('dis-flex');
+    $('#'+val).addClass('dis-flex');
     // console.log('#'+val);
     // if($(this).val() == "SQUARE,LANDSCAPE"){
     //   $(this).parents(".tab__content").siblings(".js-second").show();
