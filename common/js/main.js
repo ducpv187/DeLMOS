a136138d643f1dd90f7da905eac6bba21add0fbd
Ducpham <ducpv@beetsoft.com>
2023-02-20 13:58:13 +0700

    fix form register and up base mypage

--- common/js/main.js
+++ common/js/main.js
@@ -172,7 +172,26 @@ $(document).ready(function() {
     
   // );
   
+  //page__my--page 
+  //function tab next
 
+  // Show the first tab and hide the rest
+  $('#tabs-nav li:first-child').addClass('active');
+  $('.tab-content').hide();
+  $('.tab-content:first').show();
+
+  // Click function
+  $('#tabs-nav li').click(function(){
+    $('#tabs-nav li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-content').hide();
+    
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
+  });
+
+  
   //validate check input katana
   jQuery.validator.addMethod(
     'katakana',
