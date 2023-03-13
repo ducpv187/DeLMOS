f8c1567ff1622cefbb3ef73256a31b4167bcf7ae
Ducpham <ducpv@beetsoft.com>
2023-03-13 10:50:20 +0700

    [chang Page-login => Popup Login]

--- common/js/main.js
+++ common/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $(".btn-blue").click(function() {
+  $(".js-showpopup").click(function() {
     $(this).parents().find(".modal__popup").addClass('block');
     $(this).parents().find(".modal__content").addClass('show');
     $(this).parents().find(".modal__close").removeClass('show');
   });
-  $(".modal_close").click(function() {
+  $(".modal__close").click(function() {
     $(this).parents().find(".modal__popup").removeClass('block');
     $(this).parent(".modal__content").removeClass('show'); 
   });
